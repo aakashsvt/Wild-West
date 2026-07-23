b085c4e5ef8866296136028c381b9a269495a738
AakashShukla18 <aakash.s@virtualizetechnologies.com>
2026-07-23 17:38:22 +0530

    removed cjs files not in use

--- script/fix_newlines.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('client/src/components/PlayerController.tsx', 'utf8');
-code = code.replace(/\\n/g, '\n');
-fs.writeFileSync('client/src/components/PlayerController.tsx', code);
